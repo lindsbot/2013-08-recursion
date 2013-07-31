a9b8ff221747c27507cfd33a09a09f2f302950c3
Lindsay Hertz <lindsay.hertz@gmail.com>
2013-07-30 19:05:47 -0700

    First attempt, doesn't work

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,6 +4,29 @@
 // };
 
 // But in stead we're going to implement it from scratch:
-var getElementsByClassName = function (className) {
+var getElementsByClassName = function(className) {
   // your code here
+  var elements = [];
+  var elem = document.getElementsByTagName('body')[0];
+
+
+  // base case
+  if (elem.hasChildNodes() === false) {
+  	if (elem.className === className) {
+  		return elements.push(elem);
+  	}
+  }
+
+  else {
+  	if (elem.className === className) {
+  		elements.push(elem);
+  	}
+  	var len = elem.childNodes.length;
+  	for (var i = 0; i < len; i++) {
+		getElementsByClassName(className);
+  	}
+  }
+
+  return elements;
+
 };
