31eca49f3153b07c9d815341afc14872db4fa594
Hack Reactor Students <github@hackreactor.com>
2013-08-05 16:38:47 -0700

    "Passes tests through arrays"

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,4 +4,29 @@
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
   // your code goes here
+  if (typeof obj === 'string'){
+    return '\"' + obj + '\"';
+  }else if (typeof obj === 'boolean'){
+    return obj + '';
+  }else if (typeof obj === 'number'){
+    return obj + '';
+  }else if (obj == []){
+  	return obj + '';
+  }else if (_.isArray(obj)){
+    return '[' + _.map(obj, stringifyJSON).join(',') + ']';
+  }else if (obj == {}){
+  	return '{}';
+  }else if (obj == null){
+  	return "null";
+  }else if (typeof obj === 'object'){
+  	for (var key in obj){
+  	  return '{' + stringifyJSON(key) + '}';
+  	}
+  }else{
+  	return obj + "";
+  }
+
+
+
+
 };
